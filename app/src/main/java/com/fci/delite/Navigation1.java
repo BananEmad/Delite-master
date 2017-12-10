2cdd5c47a1d42726a8a7d2ba16c11727d08d5bb8
Rawamkamel <rawan.kamel12@gmail.com>
2017-12-10 16:06:00 +0200

    rawan30/11

--- app/src/main/java/com/fci/delite/Navigation1.java
+++ app/src/main/java/com/fci/delite/Navigation1.java
@@ -72,17 +72,21 @@ public class Navigation1 extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_Primery) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_Favourit) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_order) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_Payment) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_Feed) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_Help) {
+
+        }else if (id == R.id.nav_About) {
+
+        }else if (id == R.id.nav_place) {
 
         }
 
